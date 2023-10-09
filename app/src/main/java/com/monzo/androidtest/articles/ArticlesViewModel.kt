c60bc0165f6b2527960789784fbbd1f10383523f
Canato <vcanato@gmail.com>
2023-10-09 11:14:39 +0100

    Terminal Refresh feature fixed

--- app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/monzo/androidtest/articles/ArticlesViewModel.kt
@@ -7,29 +7,30 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ArticlesViewModel(
-        private val repository: ArticlesRepository
+    private val repository: ArticlesRepository
 ) : BaseViewModel<ArticlesState>(ArticlesState()) {
     init {
         disposables += repository.latestFintechArticles()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { articles ->
-                    setState { copy(refreshing = false, articles = articles) }
-                }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { articles ->
+                setState { copy(refreshing = false, articles = articles) }
+            }
     }
 
     fun onRefresh() {
-        setState { copy(refreshing = true) }
         disposables += repository.latestFintechArticles()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { articles ->
-                    setState { copy(articles = articles) }
-                }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ articles ->
+                setState { copy(refreshing = false, articles = articles) }
+            }, { error ->
+                // Handle error here, and set refreshing to false
+                setState { copy(refreshing = false) }
+            })
     }
 }
-
 data class ArticlesState(
-        val refreshing: Boolean = true,
-        val articles: List<Article> = emptyList()
+    val refreshing: Boolean = false,
+    val articles: List<Article> = emptyList()
 )
